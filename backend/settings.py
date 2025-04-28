6f3921c0c6e530fd9cefeecec11f8c698baf147b
yousuffaraz006 <yousuffaraz006@gmail.com>
2025-04-28 10:09:17 +0530

    Static settings removed

--- backend/settings.py
+++ backend/settings.py
@@ -74,9 +74,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
@@ -124,12 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-if DEBUG:
-        STATICFILES_DIRS = [os.path.join(BASE_DIR, 	'projectapp/static')]
-else:
-        STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_URL = '/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- backend/urls.py
+++ backend/urls.py
@@ -23,5 +23,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("project_app.urls")),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
